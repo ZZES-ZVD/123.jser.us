914921f92ad3e9f0270ad6c6a987cbf982f99eb0
zzes <kuanglong0312@sina.com>
2017-08-29 13:26:31 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
-    var arr = document.getElementById("description")
-    arr = arr.innerText.substr(0, 10) + "..."
-    $('.description').value = arr
-        // document.getElementById("description").innerHTML = arr
+    var arr = $('.description')
+    console.log(arr)
+    for(var i =0 , len = arr.length;i < len;i++){
+    	var j = arr[i].innerHTML
+    	console.log(j+"  "+j.length)
+    	if (j.length>30) {
+    		j = j.substr(0, 30) + "..."	
+    	}
+    	console.log("转换后："+j)
+    	$('.description')[i].innerHTML = j
+    }
 });
